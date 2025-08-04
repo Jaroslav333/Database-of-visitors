429360ab590809de145354907a8a8ffe2df735a3
Jaroslav <borobudur3333@gmail.com>
2025-08-05 00:11:39 +0200

    Definition frames, addition heading in main.py

--- main.py
+++ main.py
@@ -6,6 +6,29 @@ root.geometry('500x400+200+100')
 root.title('Visitors')
 root.resizable(False, False)
 
+# FRAMES
+general_frame = Frame(root)
+general_frame.pack()
+
+heading_input_frame = Frame(root)
+heading_input_frame.pack()
+
+input_frame = Frame(root)
+input_frame.pack()
+
+filter_frame = Frame(root)
+filter_frame.pack()
+
+button_frame = Frame(root)
+button_frame.pack()
+
+content_frame = Frame(root)
+content_frame.pack()
+
+## Heading
+general_label = Label(general_frame, text='Database of visitors', font=('Times New Roman', 15, 'bold'))
+general_label.pack()
+
 
 
 
