7ccc050ff9562e91014f4f8edb46ec303d6dac87
Jaroslav <borobudur3333@gmail.com>
2025-08-29 00:25:02 +0200

    Styling app in customtkinter

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 from tkinter import *
 import psycopg
 from tkinter import messagebox
+import customtkinter
 
-root = Tk()
-root.geometry('500x400+200+100')
+root = customtkinter.CTk()
+customtkinter.set_appearance_mode("dark")
+root.geometry('500x470+200+100')
 root.title('Visitors')
 root.resizable(False, False)
 
 # FRAMES
-general_frame = Frame(root)
+general_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 general_frame.pack()
 
-heading_input_frame = Frame(root)
+heading_input_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 heading_input_frame.pack()
 
-input_frame = Frame(root)
+input_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 input_frame.pack()
 
-heading_operations_frame = Frame(root)
+heading_operations_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 heading_operations_frame.pack()
 
-id_frame = Frame(root)
+id_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 id_frame.pack()
 
-button_frame = Frame(root)
+button_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 button_frame.pack()
 
-heading_content_frame = Frame(root)
+heading_content_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 heading_content_frame.pack()
 
-content_frame = Frame(root)
+content_frame = customtkinter.CTkFrame(root, fg_color='transparent')
 content_frame.pack()
 
 ## FUNCTIONS section
@@ -98,7 +100,7 @@ def search(id):
                 cur.execute(query, (id,))
                 selected_visitor = cur.fetchone()
                 if selected_visitor:
-                    search_listbox = Listbox(content_frame, width=20, height=1, justify='center')
+                    search_listbox = Listbox(content_frame, width=20, height=1, justify='center', font=('Arial', 15), bg='black', fg='White')
                     search_listbox.insert(0, selected_visitor)
                     search_listbox.grid(row=0, column=0)
                 else:
@@ -114,7 +116,7 @@ def search(id):
         print(f'Error: {e}')
 
 # listbox to display all visitors - part of content section 
-listbox = Listbox(content_frame, width=30, height=5)
+listbox = Listbox(content_frame, width=40, height=5, font=('Arial', 15), bg='black', fg='White')
 listbox.grid(row=1, column=0)
 
 scrollbar = Scrollbar(content_frame)
@@ -205,60 +207,60 @@ def update(first_name, second_name, age, id):
             
    
 ## Heading
-general_label = Label(general_frame, text='Database of visitors', font=('Times New Roman', 17, 'bold'))
-general_label.pack()
+general_label = customtkinter.CTkLabel(general_frame, text='Database of visitors', font=('Times New Roman', 25, 'bold'))
+general_label.pack(pady=(5, 10))
 
 ## Input section
-heading_visitors_label = Label(heading_input_frame, text="Visitor's registration", font=('Arial', 12, 'bold'))
-heading_visitors_label.pack()
+heading_visitors_label = customtkinter.CTkLabel(heading_input_frame, text="Visitor's registration", font=('Arial', 17, 'bold'))
+heading_visitors_label.pack(pady=(0, 5))
 
-first_name_label = Label(input_frame, text='First name: ')
+first_name_label = customtkinter.CTkLabel(input_frame, text='First name: ')
 first_name_label.grid(row=0, column=0)
 
-first_name_entry = Entry(input_frame)
-first_name_entry.grid(row=0, column=1)
+first_name_entry = customtkinter.CTkEntry(input_frame, fg_color='black')
+first_name_entry.grid(row=0, column=1, pady=(0, 3))
 
-second_name_label = Label(input_frame, text='Second name: ')
+second_name_label = customtkinter.CTkLabel(input_frame, text='Second name: ')
 second_name_label.grid(row=1, column=0)
 
-second_name_entry = Entry(input_frame)
-second_name_entry.grid(row=1, column=1)
+second_name_entry = customtkinter.CTkEntry(input_frame, fg_color='black')
+second_name_entry.grid(row=1, column=1, pady=(0, 3))
 
-age_label = Label(input_frame, text='Age: ')
+age_label = customtkinter.CTkLabel(input_frame, text='Age: ')
 age_label.grid(row=2, column=0)
 
-age_entry = Entry(input_frame)
-age_entry.grid(row=2, column=1)
+age_entry = customtkinter.CTkEntry(input_frame, fg_color='black')
+age_entry.grid(row=2, column=1, pady=(0, 5))
 
-button_register = Button(input_frame, text='Register', command=lambda:insert_data(first_name_entry.get(), second_name_entry.get(), age_entry.get()))
+button_register = customtkinter.CTkButton(input_frame, text='Register', width=30, command=lambda:insert_data(first_name_entry.get(), second_name_entry.get(), age_entry.get()))
 button_register.grid(row=3, column=1)
 
 ## Operations section
-operatins_heading_label = Label(heading_operations_frame, text='Operations', font=('Arial', 12, 'bold'))
-operatins_heading_label.pack()
+operatins_heading_label = customtkinter.CTkLabel(heading_operations_frame, text='Operations', font=('Arial', 17, 'bold'))
+operatins_heading_label.pack(pady=(5, 2))
 
-id_label = Label(id_frame, text="Visitor's ID: ")
+id_label = customtkinter.CTkLabel(id_frame, text="Visitor's ID: ")
 id_label.grid(row=0, column=0)
 
-id_entry = Entry(id_frame)
-id_entry.grid(row=0, column=1)
+id_entry = customtkinter.CTkEntry(id_frame, fg_color='black')
+id_entry.grid(row=0, column=1, pady=(0, 5))
 
 # Button section
-button_search = Button(button_frame, text='Search', command=lambda:search(id_entry.get() if id_entry.get().strip() else None))
-button_search.grid(row=0, column=0)
+button_search = customtkinter.CTkButton(button_frame, text='Search', width=30, command=lambda:search(id_entry.get() if id_entry.get().strip() else None))
+button_search.grid(row=0, column=0, padx=(0, 5))
 
-button_delete = Button(button_frame, text='Delete', command=lambda:delete(id_entry.get() if id_entry.get().strip() else None))
-button_delete.grid(row=0, column=1)
+button_delete = customtkinter.CTkButton(button_frame, text='Delete', width=30, command=lambda:delete(id_entry.get() if id_entry.get().strip() else None))
+button_delete.grid(row=0, column=1, padx=(0, 5))
 
-button_update = Button(button_frame, text='Update', command=lambda:update(first_name_entry.get(), second_name_entry.get(), age_entry.get(), id_entry.get()))
-button_update.grid(row=0, column=2)
+button_update = customtkinter.CTkButton(button_frame, text='Update', width=30, command=lambda:update(first_name_entry.get(), second_name_entry.get(), age_entry.get(), id_entry.get()))
+button_update.grid(row=0, column=2, padx=(0, 5))
 
-button_reload = Button(button_frame, text='Show changes', command=lambda:display_all_visitors())
+button_reload = customtkinter.CTkButton(button_frame, text='Show changes', width=30, command=lambda:display_all_visitors())
 button_reload.grid(row=0, column=3)
 
 # Content section
-heading_content_label = Label(heading_content_frame, text='All visitors', font=('Arial', 12, 'bold'))
-heading_content_label.pack()
+heading_content_label = customtkinter.CTkLabel(heading_content_frame, text='All visitors', font=('Arial', 17, 'bold'))
+heading_content_label.pack(pady=(5, 2))
 
 
 
